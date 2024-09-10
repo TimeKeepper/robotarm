1acb4d86c48f47f3fbe727d6520b02aeadb3d86d
Wen_Jiu <2784307979@qq.com>
2024-09-11 05:16:40 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# 定义一个机械臂节点类
+class RobotArmNode:
+    def __init__(self, ID, angle_range):
+        self.ID = ID
+        self.angle_range = angle_range
+    def move(self, angle):
+        print("机械臂{}移动到角度{}".format(self.ID, angle))
+
+def anti_movecalculation(position):
+    x, y, z = position
+
+    
